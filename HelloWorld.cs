05b78e7af1ed34ecee0a7e516ea579e9502e6a63
Jimwell040 <jimwellibay040@gmail.com>
2022-08-17 17:51:15 +0800

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpShell
+{
+	class HelloWorld
+	{
+		static void Main(string[] args) {
+			
+			Console.WriteLine("Hello, World");
+			
+		}
+	}
+}
